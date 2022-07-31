f0e11b9e0161b308277cfc8f8df328af0f1fdd29
Ingrid <ingrid.chi77@gmail.com>
2022-08-01 03:41:55 +0800

    bind admin-nav-tab API

--- src/components/AdminNavTab.vue
+++ src/components/AdminNavTab.vue
@@ -1,38 +1,53 @@
 <template>
   <div class="adminNavTab-wrapper">
-
     <div class="adminNavTab">
-        <div class="adminNavTab__menu"
-        v-for="adminMenuItem in adminMenuItems" 
-          :key="adminMenuItem.id">
-          <img 
+      <div
+        class="adminNavTab__menu"
+        v-for="adminMenuItem in adminMenuItems"
+        :key="adminMenuItem.id"
+        @click="changeTab(adminMenuItem.id)"
+      >
+        <img
           class="adminNavTab__menu__img"
-          :src="getImg(adminMenuItem.image)" alt="" />
-          <h5 class="adminNavTab__menu__title">{{ adminMenuItem.title }}</h5>
-        </div>
+          :src="getImg(adminMenuItem.image)"
+          alt=""
+        />
+        <router-link :to="adminMenuItem.path">
+          <h5
+            :class="[
+              'adminNavTab__menu__title',
+              { checked: adminMenuItem.id === currentTagId },
+            ]"
+          >
+            {{ adminMenuItem.title }}
+          </h5>
+        </router-link>
+      </div>
     </div>
-
   </div>
 </template>
 
 <script>
-import { adminMenuItems } from '../configs/contentConfigs';
+import { adminMenuItems } from "../configs/contentConfigs";
 
 export default {
   name: "AdminNavTab",
 
   data() {
     return {
-    adminMenuItems: adminMenuItems,
-    }
+      adminMenuItems: adminMenuItems,
+      currentTagId: 1,
+    };
   },
 
-    methods: {
+  methods: {
     getImg(img) {
       return require(`../assets/${img}`);
     },
+    changeTab(tabId) {
+      this.currentTagId = tabId;
+    },
   },
-
 };
 </script>
 
@@ -47,6 +62,7 @@ export default {
     align-items: center;
     color: $primary-black;
     padding: 0 0 40px 17px;
+    cursor: pointer;
     &__img {
       width: 20px;
       height: 20px;
@@ -58,8 +74,7 @@ export default {
       &.checked {
         color: $main-orange;
       }
-    }  
+    }
   }
- 
 }
 </style>
