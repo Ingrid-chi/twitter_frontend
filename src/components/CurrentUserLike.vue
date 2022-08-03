7c21c5eaa2e39f279543ead64fc02110414eecfc
Ingrid <ingrid.chi77@gmail.com>
2022-08-03 16:41:59 +0800

    add CurrentUserLike-content

---
+++ src/components/CurrentUserLike.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="currentUserLike-wrapper">
+    
+    <div class="like-container">
+      <div class="like-container__avatar">
+        <img src="./../assets/logo-gray.png" alt="" />
+      </div>
+
+      <div class="like-container__detail">
+        <!-- title -->
+        <div class="like-container__detail__title">
+          <label class="like-container__detail__title__name primary-bold"
+            >John Doe</label
+          >
+          <label class="like-container__detail__title__account"
+            >@heyjohn．</label
+          >
+          <label class="like-container__detail__title__created-at"
+            >3小時
+          </label>
+        </div>
+
+        <!-- description -->
+        <p class="like-container__detail__description">
+          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
+          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
+          reprehenderit elit laborum.
+        </p>
+
+        <!-- reply & like icon -->
+        <div class="like-container__detail__count-panel">
+          <!-- reply icon -->
+          <div class="like-container__detail__count-panel__reply">
+            <div class="like-container__detail__count-panel__reply__icon">
+              <img src="./../assets/replied.png" alt="" />
+            </div>
+            <div class="like-container__detail__count-panel__reply__count">
+              13
+            </div>
+          </div>
+
+          <!-- like icon -->
+          <div class="like-container__detail__count-panel__like">
+            <div class="like-container__detail__count-panel__like__icon">
+              <img src="./../assets/like-checked.png" alt="" />
+            </div>
+            <div class="like-container__detail__count-panel__like__count">
+              76
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="currentUserLike-wrapper__bottom"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CurrentUserLike",
+
+  components: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.currentUserLike-wrapper {
+  &__bottom {
+    @extend %line-bottom;
+  }
+}
+
+.like-container {
+  display: flex;
+  padding: 16px 24px;
+  &__avatar {
+    img {
+      width: 50px;
+      height: 50px;
+    }
+  }
+
+  &__detail {
+    padding-left: 8px;
+    &__title {
+      padding-bottom: 8px;
+      &__name {
+        @extend %primary-p;
+        color: $main-black;
+        padding-right: 8px;
+      }
+      &__account,
+      &__created-at {
+        @extend %secondary-p;
+        color: $secondary-gray;
+      }
+    }
+
+    &__description {
+      @extend %secondary-p;
+      color: $main-black;
+      line-height: 26px;
+      padding-bottom: 8px;
+    }
+
+    // reply & like icon
+    &__count-panel {
+      display: flex;
+      align-items: center;
+      &__reply {
+        display: flex;
+        align-items: center;
+        padding-right: 40px;
+        &__icon {
+          img {
+            width: 13.17px;
+            height: 13.17px;
+          }
+        }
+        &__count {
+          @extend %secondary-p;
+          color: $secondary-gray;
+          padding-left: 9px;
+        }
+      }
+
+      &__like {
+        display: flex;
+        align-items: center;
+        &__icon {
+          img {
+            width: 13.4px;
+            height: 12.61px;
+          }
+        }
+        &__count {
+          @extend %secondary-p;
+          color: $secondary-gray;
+          padding-left: 9px;
+        }
+      }
+    }
+  }
+}
+</style>
