55bdccfd52b79416899b0606cf052476286e5a46
Ingrid <ingrid.chi77@gmail.com>
2022-08-05 03:35:26 +0800

    add CurrentUserLike+API

--- src/components/CurrentUserLike.vue
+++ src/components/CurrentUserLike.vue
@@ -1,66 +1,163 @@
 <template>
   <div class="currentUserLike-wrapper">
-    
-    <div class="like-container">
-      <div class="like-container__avatar">
-        <img src="./../assets/logo-gray.png" alt="" />
-      </div>
-
-      <div class="like-container__detail">
-        <!-- title -->
-        <div class="like-container__detail__title">
-          <label class="like-container__detail__title__name primary-bold"
-            >John Doe</label
-          >
-          <label class="like-container__detail__title__account"
-            >@heyjohn．</label
-          >
-          <label class="like-container__detail__title__created-at"
-            >3小時
-          </label>
+    <div
+      class="like-container-for"
+      v-for="likedTweet in likedTweets"
+      :key="likedTweet.id"
+    >
+      <div class="like-container">
+        <div class="like-container__avatar">
+          <img :src="likedTweet.Tweet.User.avatar" alt="" />
         </div>
 
-        <!-- description -->
-        <p class="like-container__detail__description">
-          Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco
-          cillum dolor. Voluptate exercitation incididunt aliquip deserunt
-          reprehenderit elit laborum.
-        </p>
-
-        <!-- reply & like icon -->
-        <div class="like-container__detail__count-panel">
-          <!-- reply icon -->
-          <div class="like-container__detail__count-panel__reply">
-            <div class="like-container__detail__count-panel__reply__icon">
-              <img src="./../assets/replied.png" alt="" />
-            </div>
-            <div class="like-container__detail__count-panel__reply__count">
-              13
-            </div>
+        <div class="like-container__detail">
+          <!-- title -->
+          <div class="like-container__detail__title">
+            <label class="like-container__detail__title__name primary-bold">{{
+              likedTweet.Tweet.User.name
+            }}</label>
+            <label class="like-container__detail__title__account"
+              >{{ "@" + likedTweet.Tweet.User.account }}．</label
+            >
+            <label class="like-container__detail__title__created-at"
+              >{{ likedTweet.createdAt }}
+            </label>
           </div>
 
-          <!-- like icon -->
-          <div class="like-container__detail__count-panel__like">
-            <div class="like-container__detail__count-panel__like__icon">
-              <img src="./../assets/like-checked.png" alt="" />
+          <!-- description -->
+          <p class="like-container__detail__description">
+            {{ likedTweet.Tweet.description }}
+          </p>
+
+          <!-- reply & like icon -->
+          <div class="like-container__detail__count-panel">
+            <!-- reply icon -->
+            <div class="like-container__detail__count-panel__reply">
+              <div class="like-container__detail__count-panel__reply__icon">
+                <img src="./../assets/replied.png" alt="" />
+              </div>
+              <div class="like-container__detail__count-panel__reply__count">
+                {{ likedTweet.Tweet.repliesCount }}
+              </div>
             </div>
-            <div class="like-container__detail__count-panel__like__count">
-              76
+
+            <!-- like icon -->
+            <div class="like-container__detail__count-panel__like">
+              <button
+                v-if="likedTweet.islike"
+                @click.stop.prevent="deleteLike(likedTweet.id)"
+                class="like-container__detail__count-panel__like__icon"
+              >
+                <img src="./../assets/like-checked.png" alt="" />
+              </button>
+
+              <button
+                v-else
+                @click.stop.prevent="like(likedTweet.id)"
+                class="like-container__detail__count-panel__like__icon"
+              >
+                <img src="./../assets/like.png" alt="" />
+              </button>
+              <div class="like-container__detail__count-panel__like__count">
+                {{ likedTweet.Tweet.likesCount }}
+              </div>
             </div>
           </div>
         </div>
       </div>
+
+      <div class="currentUserLike-wrapper__bottom"></div>
     </div>
-
-    <div class="currentUserLike-wrapper__bottom"></div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import usersAPI from "../apis/users";
+import { Toast } from "../utils/helpers";
+
 export default {
   name: "CurrentUserLike",
 
   components: {},
+
+  data() {
+    return {
+      likedTweets: [],
+    };
+  },
+
+  created() {
+    console.log(this.currentUser);
+    this.fetchCurrentUserLike(this.currentUser.id);
+  },
+
+  methods: {
+    async fetchCurrentUserLike(id) {
+      try {
+        const response = await usersAPI.getUserLikes(id);
+        console.log(response);
+        this.likedTweets = response.likedTweets;
+        console.log(this.likedTweets);
+      } catch (error) {
+        const { response } = error;
+
+        if (response.data.message) {
+          Toast.fire({
+            icon: "error",
+            title: response.data.message,
+          });
+        }
+      }
+    },
+
+    async deleteLike(id) {
+      try {
+        // map 會返回一個陣列，取代掉舊的陣列
+        this.likedTweets = this.likedTweets.map((likedTweet) => {
+          if (likedTweet.id === id) {
+            return {
+              ...likedTweet,
+              islike: false,
+            };
+          } else {
+            return likedTweet;
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: "目前無法刪除 like, 請稍後再試",
+        });
+      }
+    },
+
+    async like(id) {
+      try {
+        this.likedTweets = this.likedTweets.map((likedTweet) => {
+          if (likedTweet.id === id) {
+            return {
+              ...likedTweet,
+              islike: true,
+            };
+          } else {
+            return likedTweet;
+          }
+        });
+      } catch (error) {
+        console.log(error);
+        Toast.fire({
+          icon: "error",
+          title: "目前無法新增 like, 請稍後再試",
+        });
+      }
+    },
+  },
+
+  computed: {
+    ...mapState(["currentUser"]),
+  },
 };
 </script>
 
@@ -73,11 +170,13 @@ export default {
 
 .like-container {
   display: flex;
+  flex-direction: row;
   padding: 16px 24px;
   &__avatar {
     img {
       width: 50px;
       height: 50px;
+      border-radius: 50%;
     }
   }
 
